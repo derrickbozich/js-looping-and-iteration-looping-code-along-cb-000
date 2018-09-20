2456149bae530ce173e10f14b36409b25f817cab
derrick b <dbozich1@gmail.com>
2018-09-20 05:12:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ function tailsNeverFails() {
 
   let count = 0;
   while(Math.random() >= 0.5){
-
       count ++;
-
   }
-  return `You got ${count} tails in a row!`
-
+  return `You got ${count} tails in a row!`;
 }
